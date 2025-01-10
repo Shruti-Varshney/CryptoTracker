2c10f1de32b27102397b586ec30923c947011e8f
Shruti-Varshney <2002shrutivarshney@gmail.com>
2025-01-11 04:28:11 +0530

    Fetch latest data for given coin

--- crypto-status-backend/routes/api.js
+++ crypto-status-backend/routes/api.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { getStats } = require('../controllers/statsController');
 
 
 const router = express.Router();
+
+router.get('/stats', getStats);
 
 
 module.exports = router;
